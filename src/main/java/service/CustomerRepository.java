fbfb4a6b0bd05afcd5be69b094932adb15512b06
Vilhard <ville.hotakainen@gmail.com>
2022-02-22 12:36:00 +0200

    changed list to collection for customers

--- src/main/java/service/CustomerRepository.java
+++ src/main/java/service/CustomerRepository.java
@@ -3,11 +3,12 @@ package service;
 import model.Customer;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface CustomerRepository  {
-    List<Customer> getAllCustomers();
+    Collection<Customer> getAllCustomers();
     Customer getCustomerById(Integer id);
     Customer getCustomerByName(String name);
     void getPageOfCustomers(Integer limit, Integer offset);
--- src/main/java/service/CustomerRepositoryImpl.java
+++ src/main/java/service/CustomerRepositoryImpl.java
@@ -1,13 +1,33 @@
 package service;
 
+import com.example.chinook_java_assignment.datasource.ConnectionBuilder;
 import model.Customer;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
-    public List<Customer> getAllCustomers() {
+    public Collection<Customer> getAllCustomers() {
+        List<Customer> customers = new ArrayList<>();
+        try {
+            Connection conn = ConnectionBuilder.getInstance().getConnection();
+            String query = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email  FROM Customer";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -18,7 +38,19 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public Customer getCustomerByName(String name) {
-    return null;
+        Connection conn = ConnectionBuilder.getInstance().getConnection();
+        String query = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email  FROM Customer WHERE FirstName = ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, name);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                //map to dto and return
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override

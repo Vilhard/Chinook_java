6b5788e7c67c94aa1519b66a10a8222618cc2700
Vilhard <ville.hotakainen@gmail.com>
2022-02-22 14:33:36 +0200

    undo Customer class implementation

--- src/main/java/com/example/chinook_java_assignment/model/Customer.java
+++ src/main/java/com/example/chinook_java_assignment/model/Customer.java
@@ -1,22 +1,21 @@
 package com.example.chinook_java_assignment.model;
 
-public record Customer(Integer CustomerId, String FirstName, String LastName, String Country, String PostalCode, String Phone, String Email) {
-    public Customer(Integer CustomerId, String FirstName, String LastName, String Country, String PostalCode,
-                    String Phone, String Email) {
-        this.CustomerId = CustomerId;
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.Country = Country;
-        this.PostalCode = PostalCode;
-        this.Phone = Phone;
-        this.Email = Email;
-    }
-    public Integer CustomerId() {return this.CustomerId;}
-    public String FirstName() {return this.FirstName;}
-    public String LastName() {return this.LastName;}
-    public String Country() {return this.Country;}
-    public String PostalCode() {return this.PostalCode;}
-    public String Phone() {return this.Phone;}
-    public String Email() {return this.Email;}
+public class Customer {
+    public Integer CustomerId;
+    public String FirstName;
+    public String LastName;
+    public String Country;
+    public String PostalCode;
+    public String Phone;
+    public String Email;
 
+    public Customer(Integer customerId, String firstName, String lastName, String country, String postalCode, String phone, String email) {
+        CustomerId = customerId;
+        FirstName = firstName;
+        LastName = lastName;
+        Country = country;
+        PostalCode = postalCode;
+        Phone = phone;
+        Email = email;
+    }
 }
